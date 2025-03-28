00335391878522a9d1bc4609b2ef96db8b87acf7
Click2Media-Subhash <subhash.paradkar@click2media.co.in>
2025-03-28 14:27:29 +0530

    Subhash: Update new Program1.py file

--- Program1.py
+++ Program1.py
@@ -4,50 +4,45 @@
 #print(10+10)
 
 ##########################Variable###############
-# name="Subhash"
-# age =34
-# Price=25.62
-# age2=age;
+name="Subhash"
+age =34
+Price=25.62
+age2=age;
 
-# print("My Name is:", age2)
+print("My Name is:", age2)
 ##########################Type###############
-# print(type(name))
-# print(type(age))
-# print(type(Price))
+print(type(name))
+print(type(age))
+print(type(Price))
 
 ##########################Variable###############
 
-# a=500
-# b=80
+a=500
+b=80
 
-# sum=a+b
-# print("Sum data",sum)
+sum=a+b
+print("Sum data",sum)
 ##########################Variable###############
 
-# print("Hello Words")
-# print("Hello Words")
-# print("Hello Words")
-# #print("Hello Words")
+a=int("5")
+b=20.5
 
-# a=int("5")
-# b=20.5
+sum=a+b
 
-# sum=a+b
-
-# print(sum)
-# print(type(a))
+print(sum)
+print(type(a))
 ##################################### Input Type basics start################
 
-# name=input("Enter your name:")
-# age=int(input("Enter your age: "))
-# marks=float(input("Enter your marks: "))
+name=input("Enter your name:")
+age=int(input("Enter your age: "))
+marks=float(input("Enter your marks: "))
 #
-# print("Welcome",name);
-# print("Your age",age);
-# print("Your marks",marks);
-# print("age type",type(name));
-# print("age type",type(age));
-# print("marks type",type(marks));
+print("Welcome",name);
+print("Your age",age);
+print("Your marks",marks);
+print("age type",type(name));
+print("age type",type(age));
+print("marks type",type(marks));
 
 ##################################### Input Type basics End################
 
